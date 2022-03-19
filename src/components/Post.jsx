1b4c856c1f53c3a3d225cf4178d5582039a34139
nhtera <nguyentien8x@gmail.com>
2022-03-19 13:09:15 +0700

    Frontend: Update upvote, downvote icon

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -124,10 +124,14 @@ export default function Post({id, title, body, up_votes, down_votes, author}) {
             <div className="flex justify-center">
                 <button onClick={upvote.bind(this, id, voteStatus)} className={`relative inline-flex text-white border-0 py-2 px-6 focus:outline-none rounded text-lg transition ${
                     voteStatus === "upvote" ? "bg-green-500 hover:bg-green-600" : "bg-gray-400"
-                  }`}>Yes <span className="absolute text-white bg-red-400 -right-4 -top-4 rounded-full w-8 h-8 text-sm p-1">{up_votes.length}</span></button> 
+                  }`}><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
+                </svg> <span className="absolute text-white bg-red-400 -right-4 -top-4 rounded-full w-8 h-8 text-sm p-1">{up_votes.length}</span></button> 
                 <button onClick={downvote.bind(this, id, voteStatus)} className={`ml-5 relative inline-flex text-white border-0 py-2 px-6 focus:outline-none rounded text-lg transition ${
                     voteStatus === "downvote" ? "bg-red-500 hover:bg-red-600" : "bg-gray-400"
-                  }`}>No <span className="absolute text-white bg-red-400 -right-4 -top-4 rounded-full w-8 h-8 text-sm p-1">{down_votes.length}</span></button>
+                  }`}><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5" />
+                </svg> <span className="absolute text-white bg-red-400 -right-4 -top-4 rounded-full w-8 h-8 text-sm p-1">{down_votes.length}</span></button>
             </div>
         </div>
     </section>
