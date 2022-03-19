14a7f8ff2ba8164122f2859d38c87c7295930ce4
nhtera <nguyentien8x@gmail.com>
2022-03-19 11:31:36 +0700

    Frontend: update form create post

--- src/pages/CreateNewPost.jsx
+++ src/pages/CreateNewPost.jsx
@@ -39,11 +39,11 @@ export default function CreatePost() {
           <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Create new post</h2>
           <div className="relative mb-4">
             <label htmlFor="title" className="leading-7 text-sm text-gray-600">Title</label>
-            <input type="title" id="title" name="title" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input required type="title" id="title" name="title" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
           <div className="relative mb-4">
             <label htmlFor="content" className="leading-7 text-sm text-gray-600">Content</label>
-            <textarea id="content" name="content" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+            <textarea required id="content" name="content" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
           </div>
           <button className="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Submit</button>
         </div>
